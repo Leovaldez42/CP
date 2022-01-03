ca4b4dc2afa5c643d5f631ee51a66f5bcde4620b
Leovaldez42 <gaurav4.037@gmail.com>
2022-01-03 17:11:37 +0530

    added duplicate zeroes

---
+++ Arrays/4_DuplicateZeros.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+        for(int i=0; i<n; i++) {
+            if(arr[i] == 0 && i+1 < n) {
+                for(int j=n-1; j>i; j--)
+                    arr[j] = arr[j-1];
+                
+                arr[++i] = 0;
+            }
+        }
+    }
+};
