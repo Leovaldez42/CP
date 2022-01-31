3b59f9f7be21ee9229875a4399286caefb8779f6
Leovaldez42 <gaurav4.037@gmail.com>
2022-01-31 14:41:42 +0530

    some leetcode problems

--- Codechef/DSA/BUYPLSE.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main () {
-    int a, b, x, y = 0;
-    cin >> a >> b >> x >> y;
-    cout << a* x + b*y;
-}
--- Leetcode/Problems/TwoSum.cpp
+++ Leetcode/Problems/TwoSum.cpp
@@ -4,36 +4,18 @@
 using namespace std;
 
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        /*
-        This is for two pointer methord
-        vector<int> result;
-        for(auto it1 = nums.begin(); it1 != nums.end(); ++it1 ) {
-            
-            auto it2 = find(it1 + 1, nums.end(), target-*it1);
-            if(it2 != nums.end()) {
-                // found the number
-                result.push_back(it1 - nums.begin());
-                result.push_back(it2 - nums.begin());
-                break;
+	public:
+		vector<int> twoSum(vector<int> &nums, int target) {
+            unordered_map<int, int> _map;
+            for(int i=0; i<nums.size(); i++) {
+                int num = nums[i];
+                int complement = target - nums[i];
+                auto it = _map.find(complement);
+                if(it == 1) {
+                    return {it->second, it>first};
+                }
+                _map[num] = i;
             }
-        }
-        return result;
-        */
-        
-        // Hashmap methord
-        unordered_map<int, int> hash;
-        for(int i=0; i<nums.size(); i++) {
-            int num = nums[i];
-            int complement = target - num;
-            auto it = hash.find(complement);
-            if(it != hash.end()) {
-                // we found
-                return {it->second, i};
-            }
-            hash[num] = i;
-        }
-        return {};
-    }
-};
+            return;
+	    }
+    };
